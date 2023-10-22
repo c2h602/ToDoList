b0e1c0af966d7f496eaa89efbb6417f9d5b5b381
c2h602 <cup.cakejr247@gmail.com>
2023-10-22 18:24:24 +0300

    Init

---
+++ ToDoList/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToDoList.Models;
+using ToDoList.Services;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json"; // Файл всегда будет лежать рядом с .exe, поэтому мы обращаемся к классу Environment (окружение)
+                                                                                             // к Сurrent directory (текущему каталогу) и будем добавлять todoDataList.json
+        private BindingList<TodoModel> _todoDataList;
+        private FileIOService _fileIOService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Window_Loaded(sender, e, _todoDataList);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e, BindingList<TodoModel> _todoDataList)
+        {
+
+            _fileIOService = new FileIOService(PATH); // в конструктор этого класса необходимо передать путь, по которому должен лежать файл, то есть PATH
+
+
+            // Если мы вдруг получаем исключение, то эта информация выводится пользлвателю, и приложение закрывается
+            try
+            {
+                _todoDataList = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+
+
+            _todoDataList = _fileIOService.LoadData();
+
+
+            dgTodoList.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;
+
+
+        }
+
+        private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
+
+        }
+    }
+}
---
+++ ToDoList/Services/FileIOService.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Models;
+
+
+
+
+/* В этом классе будет реализовано два метода: 
+ * 1 Метод, который будет считывать данные с жд
+ * 2 Метод, сохраняющий данные на жд */
+
+
+namespace ToDoList.Services
+{
+    internal class FileIOService
+    {
+
+        private readonly string PATH;
+
+        public FileIOService(string path) // Передаем путь, по которому будет сохраняться файл
+        {
+            PATH = path;
+        }
+
+
+        public BindingList<TodoModel> LoadData() // Загрузка файла: 
+                                                  
+        {
+            bool fileExists = File.Exists(PATH);  
+
+            if (!fileExists)                     // 1 Проверка на существование перед чтением файла
+            {                                    // 2 Результат проверки на сущ будет хранится в булевой переменной fileExist
+                File.CreateText(PATH).Dispose(); // 3 Если файла не сущ, то считать файл мы не можем, тогда создаем этот файл и биндинг лист с пустым списком и возвращаем его
+                return new BindingList<TodoModel>();
+            }
+
+            
+
+
+            using (var reader = File.OpenText(PATH)) // 4 Если файл сущ, то выполняем чтение с помощью .OpenText,
+            {
+                var fileText = reader.ReadToEnd(); // данные считываем в строку,
+                return JsonConvert.DeserializeObject<BindingList<TodoModel>>(fileText); // и десериализируем с помощью класса JsonConvert в коллекцию BindingList,
+                                                                                        // которая содержит модель записи  
+            }
+        }
+
+        public void SaveData(object todoDataList) // Метод, сохраняющий данные в файл
+        {
+            using (StreamWriter writer = File.CreateText(PATH)) /* Используем объект writer класса StreamWriter, который создаем путем вызова метода CreateText у класс File,
+                                                                 * при этом передаем переменную PATH, в которой хранится путь к файлу.
+                                                                 * using используется для того, чтобы вызвать метод Dispose объекта writer, который освобождает ресурсы для записи на диск */
+            {
+                string output = JsonConvert.SerializeObject(todoDataList); // Список дел десериализуется с помощью джсон конвертера в строку
+                writer.Write(output); // А здесь эта строка записывается в файл
+            }
+        }
+
+    
+    }
+}
